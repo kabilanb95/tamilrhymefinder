6e0e994fd94d8fbaa97c61dfeb630b32c1ca9b15
kabilanb95 <kabilanb95@gmail.com>
2022-08-03 00:14:01 +0530

    issue fix

--- src/main/java/com/arkmusic/tamilrhymefinder/server/ResourceUtil.java
+++ src/main/java/com/arkmusic/tamilrhymefinder/server/ResourceUtil.java
@@ -9,8 +9,7 @@ public class ResourceUtil
 {
 	public static String getFilePath(String relative_file_path_from_resources_folder)
 	{
-		return "/Users/kabilan-5523/Documents/myherokuapps/tamilrhymefinder/src/main/resources/"+relative_file_path_from_resources_folder;
-//		return ResourceUtil.class.getClassLoader().getResource(relative_file_path_from_resources_folder).getFile();
+		return ResourceUtil.class.getClassLoader().getResource(relative_file_path_from_resources_folder).getFile();
 	}
 	
 	public static String getFileAsStringFromResourcesFolder(String relative_file_path_from_resources_folder) throws IOException
