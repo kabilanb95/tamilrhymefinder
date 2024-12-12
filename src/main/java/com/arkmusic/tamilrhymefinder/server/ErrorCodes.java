fd0a891bd021e33c507adb0cb61e1cbe096246ff
kabilanb95 <kabilanb95@gmail.com>
2024-12-12 18:37:39 +0530

    Remove all existing files in this branch so that I can add my mongodb repo code

--- src/main/java/com/arkmusic/tamilrhymefinder/server/ErrorCodes.java
+++
@@ -1,6 +0,0 @@
-package com.arkmusic.tamilrhymefinder.server;
-
-public class ErrorCodes
-{
-	public static final int INVALID_PARAMETERS = 1000, PHRASE_NOT_FOUND = 1001;
-}
--- src/main/java/com/arkmusic/tamilrhymefinder/server/phrases/PhraseNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.arkmusic.tamilrhymefinder.server.phrases;
-
-public class PhraseNotFoundException extends RuntimeException
-{
-	public PhraseNotFoundException(String word)
-	{
-		super("Phrases not found for word '" + word + "'");
-	}
-}
